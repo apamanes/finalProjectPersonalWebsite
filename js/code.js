565e7c9c92db17902311199da5cd9c4c7ab24b74
apamanes <apamanesg@gmail.com>
2026-03-26 16:34:27 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -9,12 +9,55 @@ function handleScroll() {
     if (window.scrollY >= scrollLimit) {
         
         header.classList.add("sticky");
-  } else {
+
+    } else {
        
-       header.classList.remove("sticky");
-   }
+        header.classList.remove("sticky");
+
+    }
 }
 
 window.addEventListener("scroll", handleScroll);
+
+function setTheTabs(){
+
+    document.querySelectorAll(".tab-button").forEach(button =>  {
+    
+        button.addEventListener("click",() =>{
+
+            const sideBar = button.parentElement;
+            const tabsContainer = sideBar.parentElement;
+            const tabsNumber = button.dataset.forTab;
+            const tabToActivate = tabsContainer.querySelector(`.tab[data-tab="${tabsNumber}"]`);
+
+            sideBar.querySelectorAll(".tab-button").forEach(button => {
+                button.classList.remove("tab-button--active");
+            });
+
+            tabsContainer.querySelectorAll(".tab").forEach(tab => {
+                tab.classList.remove("tab--active");
+            });
+
+            button.classList.add("tab-button--active");
+            tabToActivate.classList.add("tab--active");
+
+
+        });
+
+    });
+
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    setTheTabs();
+
+});
+
+
+
+
+
 
 
